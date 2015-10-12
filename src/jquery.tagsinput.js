ab576eefe218ed542b0a30e4b79134d87a82453d
Thorsten <thorsteneckel@posteo.de>
2015-10-12 10:25:03 +0200

    Fix bug: Unquoted backspace in regular expression
    This causes an error in when trying to minify the code with some minifiers, e.g. Perl module JavaScript::Minifier.

--- src/jquery.tagsinput.js
+++ src/jquery.tagsinput.js
@@ -330,7 +330,7 @@
 				//Removes the not_valid class when user changes the value of the fake input
 				if(data.unique) {
 				    $(data.fake_input).keydown(function(event){
-				        if(event.keyCode == 8 || String.fromCharCode(event.which).match(/\w+|[áéíóúÁÉÍÓÚñÑ,/]+/)) {
+				        if(event.keyCode == 8 || String.fromCharCode(event.which).match(/\w+|[áéíóúÁÉÍÓÚñÑ,\/]+/)) {
 				            $(this).removeClass('not_valid');
 				        }
 				    });
